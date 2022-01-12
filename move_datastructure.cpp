82e8acb8a21582461c602920cbdc70e5583dc1f2
lukasn42 <koopal151@gmail.com>
2022-01-12 14:42:19 +0100

    adjusted comments

--- move_datastructure.cpp
+++ move_datastructure.cpp
@@ -11,10 +11,10 @@ bool move_datastructure<T>::is_unbalanced(pair_list_node<T>* nodePairI, pair_lis
     T l = nodePairj->v.second;
     T r = l + interval_length(nodePairj) - 1;
 
-    // if |[l,r]| < a, there cannot be >= a input intervals connected to [l,r] in the permutation graph
+    // If |[l,r]| < a, there cannot be at least a input intervals connected to [l,r] in the permutation graph.
     if (r-l+1 < a) return false;
 
-    // count the number i of input intervals connected to [l,r] in the permutation graph, stop if i == a
+    // Count the number i of input intervals connected to [l,r] in the permutation graph and stop as soon as i == a.
     uint8_t i = 1;
     while (i < a && nodePairI->sc->sc != NULL && nodePairI->sc->v.first <= r) {
         nodePairI = nodePairI->sc;
@@ -35,20 +35,27 @@ void move_datastructure<T>::balance_upto(pair_list<T> *L_in, pair_tree<T> *T_out
         nodePairIplusB = nodePairIplusB->sc;
     }
     
-    // d_1 is the maximum integer, so that [q_j, q_j + d_1 - 1] has b incoming edges in the permutation graph
+    // d_1 = p_{i+b} - q_j is the maximum integer, so that [q_j, q_j + d_1 - 1] has b incoming edges in the permutation graph.
     T d_1 = nodePairIplusB->v.first - q_j;
 
-    // create the pair (p_j + d_1, q_j + d_1), which creates two new input intervals [p_j, p_j + d_1 - 1] and [p_j + d_1, p_j + d_j - 1]
+    // Create the pair (p_j + d_1, q_j + d_1), which creates two new input intervals [p_j, p_j + d_1 - 1] and [p_j + d_1, p_j + d_j - 1].
     pair_list_node<T> n{interv_pair<T>{p_j + d_1, q_j + d_1},NULL,NULL};
     auto *nodePairNew = &T_out->insert(&n)->v;
     L_in->insert_node_after(nodePairJ,nodePairNew);
 
-    /* the output interval [q_y, q_y + d_y - 1], to which [p_j + d_1, p_j + d_j - 1] is connected to in the permutation graph, can be unbalanced
-    but if p_j + d_1 in [q_u, q_u + d_u], [q_y, q_y + d_y - 1] = [q_y, q_y + d_y - 1] and because [q_u, q_u + d_u] had b < a-1 incoming edges in the permutation graph,
-    now [q_y, q_y + d_y - 1] has at most b+1 < a incoming edges in the permutation graph, hence it is balanced
-    else if p_j + d_1 >= q_u + d_u, then it is irrelevant
-    else if it starts within [q_j, q_j + d_1 - 1] or [q_j + d_1, q_j + d_j - 1], so [q_y, q_y + d_y - 1] equals one of them
-    and because they have at most b+1 < a and a-b+1 < a incoming edges in the permutation graph, [q_y, q_y + d_y - 1] cannot be unbalanced */
+    /*
+    The output interval [q_y, q_y + d_y - 1], to which [p_j + d_1, p_j + d_j - 1] is connected in the permutation graph, can be unbalanced
+
+    Case 1: If p_j + d_1 in [q_u, q_u + d_u - 1], then [q_u, q_u + d_u - 1] = [q_y, q_y + d_y - 1]. p_j + d_1 = q_u obviously is not possible.
+            In general, once balance_upto creates a new input interval in [q_u, q_u + d_u - 1], the sequence of recursive calls ends.
+            Because of that, [q_u, q_u + d_u - 1] had b incoming edges in the permutation graph before [p_j + d_1, p_j + d_j - 1] was created.
+            Therefore after that [q_y, q_y + d_y - 1] has b+1 < a incoming edges in the permutation graph, hence it is balanced.
+
+    Case 2: Else if p_j + d_1 >= q_u + d_u, then it is irrelevant if [q_y, q_y + d_y - 1] is unbalanced, because only output intervals upto [q_u, q_u + d_u - 1] must be balanced.
+
+    Case 3: Else if p_j + d_1 lies in [q_j, q_j + d_1 - 1] or [q_j + d_1, q_j + d_j - 1], then [q_y, q_y + d_y - 1] equals one of them.
+            Because they have at most b+1 < a and a-b+1 < a incoming edges in the permutation graph, [q_y, q_y + d_y - 1] cannot be unbalanced.
+    */
     if (p_j + d_1 < q_u && !(q_j <= p_j + d_1 && p_j + d_1 < q_j + d_j)) {
         // find [q_y, q_y + d_y - 1]
         pair_list_node<T> n{interv_pair<T>{0,p_j + d_1},NULL,NULL};
@@ -70,22 +77,10 @@ template <typename T>
 move_datastructure<T>::move_datastructure(interv_seq<T> *I, T n, uint8_t a, uint8_t b) {
     k = I->size();
 
-    if (k <= 0) {
-        std::cout << "invalid value for k: (0 < k)" << std::endl;
-        return;
-    }
-    if (n < k) {
-        std::cout << "invalid value for n: (k <= n)" << std::endl;
-        return;
-    }
-    if (b < 2) {
-        std::cout << "invalid value for b: (2 <= b)" << std::endl;
-        return;
-    }
-    if (b >= a-1) {
-        std::cout << "invalid value for a: (b < a-1)" << std::endl;
-        return;
-    }
+    if (k <= 0) {std::cout << "invalid value for k: (0 < k)" << std::endl; return;}
+    if (n < k) {std::cout << "invalid value for n: (k <= n)" << std::endl; return;}
+    if (b < 2) {std::cout << "invalid value for b: (2 <= b)" << std::endl; return;}
+    if (b >= a-1) {std::cout << "invalid value for a: (b < a-1)" << std::endl; return;}
 
     this->n = n;
     this->k = k;
@@ -100,29 +95,28 @@ move_datastructure<T>::move_datastructure(interv_seq<T> *I, T n, uint8_t a, uint
         [](auto *n1, auto *n2){return n1->v.second == n2->v.second;}
     );
     
-    // build L_in and T_out
+    // Build L_in and T_out
     for (T i=0; i<k; i++) { // in O(k log k)
         pair_list_node<T> n{I->at(i),NULL,NULL};
         L_in->push_back_node(&T_out->insert(&n)->v);
     }
     pair_list_node<T> nodeN{interv_pair<T>{n,0},NULL,NULL};
-    L_in->push_back_node(&nodeN); // append pair (n, 0), so that interval_length() also works for the last pair
+    L_in->push_back_node(&nodeN); // Append pair (n,0), so that interval_length() also works for the last pair.
     delete I;
 
-    // itInp points to to the pair (p_i, q_i)
+    // itInp points to to the pair (p_i, q_i).
     auto itInp = L_in->iterator(L_in->head());
-    // itOutp points to the pair (p_j, q_j)
+    // itOutp points to the pair (p_j, q_j).
     auto itOutp = T_out->iterator(T_out->minimum());
-    // at the start of each iteration [p_i, p_i + d_i - 1] is the first input interval connected to [q_j, q_j + d_j - 1] in the permutation graph
-    // and all output intervals starting before [q_j, q_j + d_j - 1] are balanced
+    // At the start of each iteration, [p_i, p_i + d_i - 1] is the first input interval connected to [q_j, q_j + d_j - 1] in the permutation graph
+    // and all output intervals starting before [q_j, q_j + d_j - 1] are balanced.
     do {
-        // if it is unbalanced, balance [q_j, q_j + d_j - 1] and all output intervals starting before it
+        // If [q_j, q_j + d_j - 1] is unbalanced, balance it and all output intervals starting before it, that might get unbalanced in the process.
         if (is_unbalanced(itInp.current(),&itOutp.current()->v)) {
             balance_upto(L_in,T_out,itInp.current(),&itOutp.current()->v,itOutp.current()->v.v.second);
         }
 
-        // find the next output interval with an incoming edge in the permutation graph
-        // and find the first input interval connected to it in the permutation graph
+        // Find the next output interval with an incoming edge in the permutation graph and find the first input interval connected to it.
         do {
             if (!itOutp.has_next()) goto end;
             itOutp.next();
@@ -136,7 +130,7 @@ move_datastructure<T>::move_datastructure(interv_seq<T> *I, T n, uint8_t a, uint
 
     k = L_in->size() - 1;
 
-    // build D_pair
+    // Build D_pair
     D_pair.resize(k);
     auto it = L_in->iterator(L_in->head());
     for (T i=0; i<k; i++) { // in O(k)
@@ -144,15 +138,18 @@ move_datastructure<T>::move_datastructure(interv_seq<T> *I, T n, uint8_t a, uint
         it.next();
     }
 
+    // Remove the last node, because it will not get deleted when T_out gets deleted.
+    L_in->remove_node(L_in->tail());
+    // Disconnect L_in from it's nodes, because they will get deleted when T_out gets deleted.
     L_in->disconnect_from_nodes();
     delete L_in;
     delete T_out;
 
-    // build D_index
+    // Build D_index
     D_index.resize(k);
     for (T j=0; j<k; j++) { // in O(k log k)
-        // find the input interval [p_i, p_i + d_i - 1], q_j is in
-        // find the maximum integer i \in [0,k-1], so that D_pair[j].first <= D_pair[i].second
+        // Find the input interval [p_i, p_i + d_i - 1], q_j is in.
+        // Find the maximum integer i \in [0,k-1], so that D_pair[j].first <= D_pair[i].second.
         T l = 0;
         T r = k-1;
         T m;
@@ -194,7 +191,7 @@ T move_datastructure<T>::find_interval(T i) {
 }
 
 template <typename T>
-interv_pair<T> move_datastructure<T>::move(T i, T x) { // in O(a)
+interv_pair<T> move_datastructure<T>::move(T i, T x) {
     T i_ = D_pair[x].second+(i-D_pair[x].first);
     T x_ = D_index[x];
     while (i_ < D_pair[x_].first) x_++; // in O(a)
--- move_datastructure.h
+++ move_datastructure.h
@@ -32,10 +32,10 @@ class move_datastructure {
     /* number of intervals, 0 < k */
     T k;
 
-    /* minimum number of incoming edges in the permutation graph, an output interval must have, to get cut, 4 <= a, (default = 4) */
+    /* minimum number of incoming edges in the permutation graph, an output interval must have, to get cut, 4 <= a (default = 4) */
     uint8_t a;
 
-    /* number of remaining incoming edges after an output interval is cut, 2 <= b < a-1, (default = 2) */
+    /* number of remaining incoming edges after an output interval gets cut, 2 <= b < a-1 (default = 2) */
     uint8_t b;
 
     /* balanced inteval sequence B_I */
@@ -53,39 +53,39 @@ class move_datastructure {
     static inline T interval_length(pair_list_node<T>* nodePair);
 
     /**
-     * @brief checks if the an output interval is unbalanced
+     * @brief returns whether the an output interval is unbalanced
      * 
-     * @param nodePairI pointer to the node in L_in storing (p_i,q_i), [p_i, p_i + d_i - 1] must be the first input interval connected to [q_j, q_j + d_j - 1] in the permutation graph
-     * @param nodePairJ pointer to the node in L_in storing (p_j,q_j), [q_j, q_j + d_j - 1] must be the first unbalanced output interval
+     * @param nodePairI pointer to the node in L_in, storing (p_i,q_i), [p_i, p_i + d_i - 1] must be the first input interval connected to [q_j, q_j + d_j - 1] in the permutation graph
+     * @param nodePairJ pointer to the node in L_in, storing (p_j,q_j), [q_j, q_j + d_j - 1] must be the first unbalanced output interval
      * @return true if [q_j, q_j + d_j - 1] is unbalanced (has >= a incoming edges in the permutation graph)
      * @return false if [q_j, q_j + d_j - 1] is balanced(has < a incoming edges in the permutation graph)
      */
     inline bool is_unbalanced(pair_list_node<T>* nodePairI, pair_list_node<T>* nodePairJ);
 
     /**
-     * @brief balances an output interval and all by that newly created unbalanced output intervals starting before q_u
+     * @brief balances an output interval and all unbalanced output intervals starting before or at q_u, that have got unbalanced in the process
      * 
      * @param L_in pointer to L_in
      * @param T_out pointer to T_out
-     * @param nodePairI pointer to the node in L_in storing (p_i,q_i), [p_i, p_i + d_i - 1] must be the first input interval connected to [q_j, q_j + d_j - 1] in the permutation graph
-     * @param nodePairJ pointer to the node in L_in storing (p_j,q_j), [q_j, q_j + d_j - 1] must be the first unbalanced output interval
+     * @param nodePairI pointer to the node in L_in, storing (p_i,q_i), [p_i, p_i + d_i - 1] must be the first input interval connected to [q_j, q_j + d_j - 1] in the permutation graph
+     * @param nodePairJ pointer to the node in L_in, storing (p_j,q_j), [q_j, q_j + d_j - 1] must be the first unbalanced output interval
      * @param q_u starting position of an output interval
      */
     void balance_upto(pair_list<T> *L_in, pair_tree<T> *T_out, pair_list_node<T>* nodePairI, pair_list_node<T>* nodePairJ, T q_u);
 
     public:
     /**
-     * @brief creates move datastructure out of I by building L_in and T_out from I, balancing I and then building D_pair and D_index; in O(k log k)
+     * @brief creates a move datastructure out of I by building L_in and T_out from I, balancing I and then building D_pair and D_index, in O(k log k)
      * 
      * @param I disjoint interval sequence I
      * @param n n = p_{k-1} + d_{k-1}, k <= n
-     * @param a minimum number of incoming edges in the permutation graph, an output interval must have, to get cut, 4 <= a, (default = 4)
-     * @param b number of remaining incoming edges after an output interval is cut, 2 <= b < a-1, (default = 2)
+     * @param a minimum number of incoming edges in the permutation graph, an output interval must have, to get cut, 4 <= a (default = 4)
+     * @param b number of remaining incoming edges after an output interval is cut, 2 <= b < a-1 (default = 2)
      */
     move_datastructure(interv_seq<T> *I, T n, uint8_t a = 4, uint8_t b = 2);
 
     /**
-     * @brief returns the number of intervals
+     * @brief returns the number of intervals in D_pair
      * 
      * @return T k
      */
@@ -100,7 +100,7 @@ class move_datastructure {
     T find_interval(T i);
 
     /**
-     * @brief calculates the move query Move(I,i,x), in O(a)
+     * @brief calculates the move query Move(I,i,x) = (i',x'), in O(a)
      * 
      * @param i i in [0..n-1]
      * @param x x in [0..k-1] and i in [p_x, p_x + d_x - 1]
